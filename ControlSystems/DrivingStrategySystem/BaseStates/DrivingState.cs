6bfd926ff4f9deff7801c1b3e4922857bf0310d3
Eli KA <elijahasiamah142004@gmail.com>
2025-01-15 21:06:52 +0100

    Update DrivingState.cs
    Implemented a public static instance to prevent repeating of class instantiation during state transitions, ensuring that the same instance is reused whenever switching states

--- ControlSystems/DrivingStrategySystem/BaseStates/DrivingState.cs
+++ ControlSystems/DrivingStrategySystem/BaseStates/DrivingState.cs
@@ -3,16 +3,22 @@ using RobotProject.ControlSystems.Util;
 
 namespace RobotProject.ControlSystems.DrivingStrategySystem.BaseStates;
 
-public abstract class DrivingState : IObjectDetectionState
+public abstract class DrivingState<T> : IDrivingState where T : DrivingState<T>, new()
 {
-    public abstract void Handle(DrivingSystem system);
+    private static T _instance;
+
+    public static T Instance => _instance ??= new T(); // Singleton instance
+
+    public abstract void Handle(DrivingSystem system, DrivingContext context);
 
     protected void Stop(DrivingSystem system)
     {
         system.Stop();
     }
+
     protected void Drive(DrivingSystem system, Direction direction, double speed)
     {
         system.Drive(direction, speed);
     }
 }
+
